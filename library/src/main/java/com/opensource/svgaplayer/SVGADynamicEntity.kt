6dfd5597fa9e7ba4735610fb1385a3353bc3c43a
poqiao <1162667628@qq.com>
2024-11-04 17:05:57 +0800

    dev

--- library/src/main/java/com/opensource/svgaplayer/SVGADynamicEntity.kt
+++ library/src/main/java/com/opensource/svgaplayer/SVGADynamicEntity.kt
@@ -1,5 +1,6 @@
 package com.opensource.svgaplayer
 
+import android.R.attr
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
@@ -7,12 +8,12 @@ import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.graphics.Rect
-import android.graphics.RectF
 import android.text.BoringLayout
 import android.text.StaticLayout
 import android.text.TextPaint
 import java.net.HttpURLConnection
 import java.net.URL
+
 
 /**
  * Created by cuiminghui on 2017/3/30.
@@ -34,7 +35,7 @@ class SVGADynamicEntity {
     internal var dynamicDrawer: HashMap<String, (canvas: Canvas, frameIndex: Int) -> Boolean> = hashMapOf()
 
     //点击事件回调map
-    internal var mClickMap : HashMap<String, IntArray> = hashMapOf()
+    internal var mClickMap: HashMap<String, IntArray> = hashMapOf()
     internal var dynamicIClickArea: HashMap<String, IClickAreaListener> = hashMapOf()
 
     internal var dynamicDrawerSized: HashMap<String, (canvas: Canvas, frameIndex: Int, width: Int, height: Int) -> Boolean> = hashMapOf()
@@ -46,17 +47,16 @@ class SVGADynamicEntity {
         this.dynamicHidden.put(forKey, value)
     }
 
-    fun setDynamicImage(bitmap: Bitmap, forKey: String,isCircle: Boolean=false) {
-        if (isCircle)
-        {
+    fun setDynamicImage(bitmap: Bitmap, forKey: String, isCircle: Boolean = false) {
+        if (isCircle) {
             this.dynamicImage.put(forKey, toRoundBitmap(bitmap))
-        }else{
-            this.dynamicImage.put(forKey,bitmap)
+        } else {
+            this.dynamicImage.put(forKey, bitmap)
         }
 
     }
 
-    fun setDynamicImage(url: String, forKey: String,isCircle: Boolean=false) {
+    fun setDynamicImage(url: String, forKey: String, isCircle: Boolean = false) {
         val handler = android.os.Handler()
         SVGAParser.threadPoolExecutor.execute {
             (URL(url).openConnection() as? HttpURLConnection)?.let {
@@ -66,7 +66,7 @@ class SVGADynamicEntity {
                     it.connect()
                     it.inputStream.use { stream ->
                         BitmapFactory.decodeStream(stream)?.let {
-                            handler.post { setDynamicImage(it,forKey,isCircle) }
+                            handler.post { setDynamicImage(it, forKey, isCircle) }
                         }
                     }
                 } catch (e: Exception) {
@@ -95,8 +95,8 @@ class SVGADynamicEntity {
 
     fun setDynamicText(layoutText: BoringLayout, forKey: String) {
         this.isTextDirty = true
-        BoringLayout.isBoring(layoutText.text,layoutText.paint)?.let {
-            this.dynamicBoringLayoutText.put(forKey,layoutText)
+        BoringLayout.isBoring(layoutText.text, layoutText.paint)?.let {
+            this.dynamicBoringLayoutText.put(forKey, layoutText)
         }
     }
 
@@ -105,13 +105,13 @@ class SVGADynamicEntity {
     }
 
     fun setClickArea(clickKey: List<String>) {
-        for(itemKey in clickKey){
-            dynamicIClickArea.put(itemKey,object : IClickAreaListener {
+        for (itemKey in clickKey) {
+            dynamicIClickArea.put(itemKey, object : IClickAreaListener {
                 override fun onResponseArea(key: String, x0: Int, y0: Int, x1: Int, y1: Int) {
                     mClickMap.let {
-                        if(it.get(key) == null){
-                            it.put(key, intArrayOf(x0,y0,x1,y1))
-                        }else{
+                        if (it.get(key) == null) {
+                            it.put(key, intArrayOf(x0, y0, x1, y1))
+                        } else {
                             it.get(key)?.let {
                                 it[0] = x0
                                 it[1] = y0
@@ -169,55 +169,19 @@ class SVGADynamicEntity {
      * @return
      */
     fun toRoundBitmap(bitmap: Bitmap): Bitmap {
-        var width = bitmap.getWidth()
-        var height = bitmap.getHeight()
-        val roundPx: Float
-        val left: Float
-        val top: Float
-        val right: Float
-        val bottom: Float
-        val dst_left: Float
-        val dst_top: Float
-        val dst_right: Float
-        val dst_bottom: Float
-        if (width <= height) {
-            roundPx = (width / 2).toFloat()
-            top = 0f
-            bottom = width.toFloat()
-            left = 0f
-            right = width.toFloat()
-            height = width
-            dst_left = 0f
-            dst_top = 0f
-            dst_right = width.toFloat()
-            dst_bottom = width.toFloat()
-        } else {
-            roundPx = (height / 2).toFloat()
-            val clip = ((width - height) / 2).toFloat()
-            left = clip
-            right = width - clip
-            top = 0f
-            bottom = height.toFloat()
-            width = height
-            dst_left = 0f
-            dst_top = 0f
-            dst_right = height.toFloat()
-            dst_bottom = height.toFloat()
-        }
-        val output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val output = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(output)
-        val color = -0xbdbdbe
+
         val paint = Paint()
-        val src = Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
-        val dst = Rect(dst_left.toInt(), dst_top.toInt(), dst_right.toInt(), dst_bottom.toInt())
-        val rectF = RectF(dst)
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
+
         paint.isAntiAlias = true
-        canvas.drawARGB(0, 0, 0, 0)
-        paint.setColor(color)
-        canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
+        canvas.drawCircle(bitmap.width / 2f, bitmap.height / 2f, bitmap.width / 2f, paint)
+
         paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
-        canvas.drawBitmap(bitmap, src, dst, paint)
-        if (!bitmap.isRecycled) {
+        canvas.drawBitmap(bitmap, rect, rect, paint)
+        if (!bitmap.isRecycled)
+        {
             bitmap.recycle()
         }
         return output
